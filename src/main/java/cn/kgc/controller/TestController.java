9b774d338243e874e2ae36c62c77a01c6e2f756b
wanglining001 <1052185069@qq.com>
2020-07-15 22:49:34 +0800

    second commit

---
+++ src/main/java/cn/kgc/controller/TestController.java
@@ -0,0 +1,12 @@
+package cn.kgc.controller;
+
+/**
+ * @author 王立宁
+ * @create 2020-07-15
+ * 类描述：
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("hello!!!!!!!!");
+    }
+}
